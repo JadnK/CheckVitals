ef6d2f687de80bf0aa5cab4d4e8b7f472f4c47e0
b45632 <jaden.kasche@swr.de>
2026-06-30 11:02:36 +0200

    feat: Added Docker Compose setup

--- backend/src/main/java/de/jadenk/checkvitals/config/CorsConfig.java
+++ backend/src/main/java/de/jadenk/checkvitals/config/CorsConfig.java
@@ -13,8 +13,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                registry.addMapping("/api/**")g
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:3000"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
