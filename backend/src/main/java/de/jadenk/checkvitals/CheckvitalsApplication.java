f8c860ce1f01c622bd819cafa3a66d1682b8af26
b45632 <jaden.kasche@swr.de>
2026-06-30 09:14:57 +0200

    feat: Initial commit of backend / frontend

---
+++ backend/src/main/java/de/jadenk/checkvitals/CheckvitalsApplication.java
@@ -0,0 +1,13 @@
+package de.jadenk.checkvitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckvitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckvitalsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/jadenk/checkvitals/CheckvitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jadenk.checkvitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckvitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
